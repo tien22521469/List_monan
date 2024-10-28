489da375e8747e8db45744e44133ea966d699274
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-10-28 21:01:32 +0700

    tao iteam food, them lib

---
+++ app/src/main/java/com/example/listview/Item.java
@@ -0,0 +1,37 @@
+package com.example.listview;
+
+public class Item {
+    private int imageResource;
+    private String title;
+    private String description;
+
+    public Item(int imageResource, String title, String description) {
+        this.imageResource = imageResource;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
